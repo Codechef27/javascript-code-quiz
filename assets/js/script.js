9df5296ea85d484c8a115f5a9e5b3011fae2815b
BradleyK <bradleyjkennett@gmail.com>
2022-05-09 10:10:48 -0400

    added query selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,47 +13,88 @@
 //THEN I can save my initials and score
 //WHEN I click the start button
 //THEN a timer starts
+
+//query selector's/ define vars
+var intro = document.querySelector("#intro");
+var introContainer = document.querySelector("#intro-container");
+var startBtn = document.querySelector('#start-btn');
+
+var questions = document.querySelector("#questions");
+var questionDisplayed = document.querySelector("#question-displayed");
+
+var questionButtons = document.querySelectorAll(".choices");
+var answerBtnA = document.querySelector("#answer-btn-A");
+var answerBtnB = document.querySelector("#answer-btn-B");
+var answerBtnC = document.querySelector("#answer-btn-C");
+var answerBtnD = document.querySelector("#answer-btn-D");
+
+var checkAnswer = document.querySelector("#check-answer");
+var report = document.querySelector("#report-section");
+var score = document.querySelector("#score");
+var enterInitials = document.querySelector("#initials");
+
+var initialsBtn = document.querySelector("#initials-btn");
+var highScoreContainer = document.querySelector("#high-score-container");
+var highScores = document.querySelector("#high-scores");
+var highScoresCheck = document.querySelector("#high-scores-check");
+var finished = document.querySelector("#finished");
+var tryAgainBtn = document.querySelector("#try-again-btn");
+var clearBtn = document.querySelector("#clear-btn");
+
 var questionsArray = [
   {
-      question: "Questions 1 : String values must be enclosed within _____ when being assigned to variables.",
-      choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
-      answer: "c"
+      question: "Questions 1 : Which event occurs when the user clicks on an HTML element?",
+      choices: ["A. onclick", "B. onchange", "C. onmouseover", "D. onmouseclick"],
+      answer: "A"
   },
   {
-      question: "Questions 2 : Commonly used data types DO NOT include:",
+      question: "Questions 2 : To see if two variables are equal in an if / else statement you would use ____.",
+      choices: ["A. =", "B. ==", "C. 'equals'", "D. !="],
+      answer: "B"
+  },
+  {
+      question: "Questions 3 : Commonly used data types DO NOT include:",
       choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
       answer: "c"
   },
   {
-      question: "Questions 3 : How do you create a function in JavaScript",
-      choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
-      answer: "b"
-  },
-  {
       question: "Questions 4 : How do you call a function named myFunction?",
-      choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
-      answer: "c"
+      choices: ["A. call myFunction()", "B. call function myFunction()", "C. myFunction()", "D. call myFunction"],
+      answer: "C"
   },
   {
-      question: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
-      choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
-      answer: "b"
+     question: "Questions 5 : How do you create a function in JavaScript",
+     choices: ["A. function = myFunction()", "B. function myFunction()", "C. function:myFunction()", "D. createMyFunction()"],
+     answer: "B"
   },
   {
-      question: "Questions 6 : The first index of an array is ____.",
-      choices: ["a. 0", "b. 1", "c. 8", "d. any"],
-      answer: "a"
+     question: "Questions 6 : String values must be enclosed within _____ when being assigned to variables.",
+     choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
+     answer: "C"
+  },
+
+  {
+      question: "Questions 7 : The first index of an array is ____.",
+      choices: ["A. 0", "B. 1", "C. 8", "D. any"],
+      answer: "A"
   },
   {
-      question: "Questions 7 : How to write an IF statement in JavaScript?",
-      choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
-      answer: "c"
+      question: "Questions 8 : How to write an IF statement in JavaScript?",
+      choices: ["A. if i == 5 then", "B. if i = 5 then", "C. if(i == 5)", "D. if i = 5"],
+      answer: "C"
   },
   {
-      question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
-      choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
-      answer: "a"
+     question: "Questions 9 : What will the code return? Boolean(3 < 7)",
+     choices: ["A. true", "B. false", "C. NaN", "D. syntaxError"],
+     answer: "A"
+
+  },
+  {
+     question: "questions 10 : Who is the best Developer in the world?",
+     choices: ["A. Ronny", "B. Gary", "C. Jonathan", "D. Charles" ],
+     answer: ["A", "B", "C", "D"]
   }
+      
 ];
 
 //WHEN I click the start button THEN a timer starts
@@ -74,9 +115,12 @@ function countdown() {
   }
 
 
-
-var startQuizEl = document.getElementById('start-btn')
-startQuizEl.addEventListener("click", function() {
+ function startQuizEl () {
   countdown();
+  introContainer.style.display = "none";
   
-});
+};
+
+//event listener's
+
+startBtn.addEventListener("click", startQuizEl);
